d901e8b71886ba32e6001c1c0b7d242441fedf19
doanvankhang <62994103+doanvankhang@users.noreply.github.com>
2021-09-05 23:52:05 +0700

    Khang bo cai sgin vo vo nhung chua xong dau

--- src/app/controllers/PatientController.js
+++ src/app/controllers/PatientController.js
@@ -4,8 +4,19 @@ const { mongooseToObject } = require('../../util/mongoose');
 
 class PatientController {
   //[GET] accountPatient/login
+
+
   login(req, res) { 
     res.render('accountPatient/login');
+  }
+
+  //[Get] accountPatient/sginin
+  sginin(req, res) { 
+    res.render('accountPatient/sginin');
+  }
+    //[POST] accountPatient/DANKy
+  danki(req, res, next) { 
+    res.json(req.body);
   }
 }
 
