af406c65f58f82909b91f25d5fbc544364020db2
unknown <ntluan1.fb@gmail.com>
2021-09-07 16:47:31 +0700

    doi giao dien

--- src/routes/medicalRecord.js
+++ src/routes/medicalRecord.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const medicalRecordController = require('../app/controllers/MedicalRecordController');
+
 router.post('/storeMedicalRecord', medicalRecordController.storeMedicalRecord);
+
 router.get('/create', medicalRecordController.create);
+
 router.post('/handle-form-action', medicalRecordController.handFormAction);
+
 router.patch('/:id/restore', medicalRecordController.restore);
+
 router.delete('/:id/xoa-that', medicalRecordController.destroyThat);
-router.get('/:id', medicalRecordController.show);
+
 router.delete('/:id', medicalRecordController.destroy);
+
+router.get('/:id', medicalRecordController.show);
+
+
 module.exports = router;
