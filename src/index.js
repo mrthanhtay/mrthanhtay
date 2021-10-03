46944ba653273ddb80f61ba74ff50a865926edde
mrthanhtay <taythanh1999@gmail.com>
2021-10-03 10:06:08 +0700

    update v5

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ const { mongooseToObject } = require('./util/mongoose');
 
 
 //testdata
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.use(cookieParser())
 const route = require('./routes');
 const db = require('./config/db');
